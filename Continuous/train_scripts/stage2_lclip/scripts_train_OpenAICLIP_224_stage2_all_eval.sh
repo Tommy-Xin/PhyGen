cf86d43e9777a0a8ad0ab5651bffc6fa7e2efaf4
Tommy-Xin <xinhku168@gmail.com>
2026-03-06 16:57:43 +0800

    add the clip loss

---
+++ Continuous/train_scripts/stage2_lclip/scripts_train_OpenAICLIP_224_stage2_all_eval.sh
@@ -0,0 +1,9 @@
+export AE="/data2/xinc/PhyGen/Continuous/pretrained_weights/FLUX.1-dev/ae.safetensors"
+
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+ROOT_DIR="/data2/xinc/PhyGen/Continuous"
+cd "${ROOT_DIR}" || exit 1
+
+accelerate launch --config_file "train_configs/accelerate_config.yaml" \
+    "training/stage2_lclip/train_OpenAICLIP_stage2_all_eval.py" \
+    --config "train_configs/stage2_lclip/test_OpenAICLIP_224_stage2_all_eval.yaml"
